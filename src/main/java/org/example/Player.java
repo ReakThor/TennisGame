8e02c4cb7501806b71982d5084561628a71a9e1d
[KK] <kovesdy.kristof@gmail.com>
2023-03-28 00:44:09 +0200

    ugly but works

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,5 +1,44 @@
 package org.example;
 
-public class Player {
+public class Player
+{
+    private String name;
+    private int score;
 
+    public Player(String name)
+    {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String printScore()
+    {
+        switch (score)
+        {
+            case 0:
+                return "love";
+            case 1:
+                return "fifteen";
+            case 2:
+                return "thirty";
+            case 3:
+                return "forty";
+        }
+        return "";
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public void winBall()
+    {
+        score++;
+    }
 }
