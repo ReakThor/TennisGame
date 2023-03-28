c26d3a502de98dd6ce10b932850a01f7fcec5a18
Adam <gubadam97@gmail.com>
2023-03-28 13:05:59 +0200

    tennisGame solution

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,5 +1,34 @@
 package org.example;
 
 public class Player {
+    private String name;
+    private int score = 0;
 
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void winBall() {
+        score++;
+    }
+    public String printScore(){
+        String scoreAsString = "";
+        if (score == 0) {
+            scoreAsString = "love";
+        } else if (score == 1) {
+            scoreAsString = "fifteen";
+        } else if (score == 2) {
+            scoreAsString = "thirty";
+        } else if (score == 3) {
+            scoreAsString = "forty";
+        }
+        return scoreAsString;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
